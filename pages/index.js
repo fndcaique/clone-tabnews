47bcae9a7574e0d73e9029b61d0a9f921f2c208b
Fernando Soares <fndcaique@gmail.com>
2025-10-02 12:28:24 -0300

    add scripts `lint` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
-	return (
-		<h1>
-			Oi Laís, estou aprendendo inglês e gostaria de te dizer: I love you {"<3"}
-		</h1>
-	);
+  return (
+    <h1>
+      Oi Laís, estou aprendendo inglês e gostaria de te dizer: I love you {'<3'}
+    </h1>
+  );
+}
+
+export function teste() {
+  return 'esse é um teste';
 }
