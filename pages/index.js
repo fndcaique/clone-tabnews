f06ef82b4d11c6a2c3561f19e8361dc4b3e14a37
Fernando Soares <fndcaique@gmail.com>
2025-10-02 11:56:49 -0300

    add file `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
-export default function Home () {
-    return <h1>Oi Laís, estou aprendendo inglês e gostaria de te dizer: I love you {'<3'}</h1>;
+export default function Home() {
+	return (
+		<h1>
+			Oi Laís, estou aprendendo inglês e gostaria de te dizer: I love you {"<3"}
+		</h1>
+	);
 }
