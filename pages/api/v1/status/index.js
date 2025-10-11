80cb9da5310aa21f25a9eca3a87c4ba78ae4cd38
Fernando Soares <fndcaique@gmail.com>
2025-10-10 22:29:59 -0300

    add `version` of database server to response of `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,13 @@ import database from 'infra/database';
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const {
+    rows: [{ server_version }],
+  } = await database.query('SHOW server_version');
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: { version: server_version },
+    },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test('GET to /api/v1/stats shout return 200', async () => {
+test('GET to /api/v1/status should return 200', async () => {
   const dateNow = new Date();
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
@@ -7,4 +7,7 @@ test('GET to /api/v1/stats shout return 200', async () => {
   const statusDate = new Date(responseBody.updated_at);
   expect(statusDate).toBeInstanceOf(Date);
   expect(statusDate.getTime()).toBeGreaterThanOrEqual(dateNow.getTime());
+  expect(responseBody.dependencies).toBeDefined();
+  expect(responseBody.dependencies.database).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe('16.0');
 });
