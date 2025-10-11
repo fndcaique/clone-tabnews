521a09c3a5efa7dcb97847deee09ad67c46f8e8f
Fernando Soares <fndcaique@gmail.com>
2025-10-10 22:23:31 -0300

    add `updated_at` to response of `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,8 @@
 import database from 'infra/database';
 
 export default async function status(request, response) {
-  const {
-    rows: [result],
-  } = await database.query('SELECT (1 + 1) as sum;');
-  console.log({ result });
-  response.status(200).json({ status: 'ok' });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test('GET to /api/v1/stats shout return 200', async () => {
+  const dateNow = new Date();
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody).toHaveProperty('updated_at');
+  const statusDate = new Date(responseBody.updated_at);
+  expect(statusDate).toBeInstanceOf(Date);
+  expect(statusDate.getTime()).toBeGreaterThanOrEqual(dateNow.getTime());
 });
